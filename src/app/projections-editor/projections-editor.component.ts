971566eb0f754929f898eed67481434a7fcf3058
Mike Scanlon <scanlom@gmail.com>
2021-06-02 11:08:22 +0800

    Default projections editor

--- src/app/projections-editor/projections-editor.component.ts
+++ src/app/projections-editor/projections-editor.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges	} from '@angular/core';
 import { FormControl } from '@angular/forms';
+import * as moment from 'moment';
 
 import { Projections } from '../projections';
 import { ProjectionsService } from '../projections.service';
@@ -9,7 +10,8 @@ import { ProjectionsService } from '../projections.service';
   templateUrl: './projections-editor.component.html',
   styleUrls: ['./projections-editor.component.css']
 })
-export class ProjectionsEditorComponent {
+export class ProjectionsEditorComponent implements OnChanges {
+  @Input() rowData: any;
   @Input() refDataId: number;
   @Input() date: string;
   @Input() eps: number;
@@ -25,6 +27,35 @@ export class ProjectionsEditorComponent {
   constructor(private projectionsService: ProjectionsService) {
 	console.log(this.projectionsService); // It is defined
  }
+
+  ngOnChanges(changes: SimpleChanges) {
+	if (changes.rowData && changes.rowData.currentValue) {
+		this.refDataId = this.rowData[0].refDataId;
+		this.date = moment().format("MM-DD-YYYY");
+		if (this.rowData[0].entryType == "O") {
+			this.eps = this.rowData[0].eps;
+			this.dps = this.rowData[0].dps;
+			this.growth = this.rowData[0].growth;
+			this.peTerminal = this.rowData[0].peTerminal;
+			this.payout = this.rowData[0].payout;
+			this.book = this.rowData[0].book;
+			this.roe = this.rowData[0].roe;
+			this.epsYr1 = this.rowData[0].epsYr1;
+			this.epsYr2 = this.rowData[0].epsYr2;
+		}
+		else {
+			this.eps = null;
+			this.dps = null;
+			this.growth = null;
+			this.peTerminal = null;
+			this.payout = null;
+			this.book = null;
+			this.roe = null;
+			this.epsYr1 = null;
+			this.epsYr2 = null;
+		}
+	}
+  }
 
   addProjections() {
 	console.log(this.refDataId)
